111acc0f0d75e40da0117ae835e6b2388568ca88
unknown <roma.baranov@gmail.com>
2020-04-24 14:37:31 +0300

    Locators and LoginPage class

---
+++ pages/login_page.py
@@ -0,0 +1,18 @@
+from pages.base_page import BasePage
+from pages.locators import LoginPageLocators
+
+class LoginPage(BasePage):
+    def should_be_login_page(self):
+        self.should_be_login_url()
+        self.should_be_login_form()
+        self.should_be_register_form()
+
+    def should_be_login_url(self):
+        #Проверка на корректный url адрес
+        assert self.browser.current_url.find("login") != -1, "No 'login' substring in url"
+
+    def should_be_login_form(self):
+        assert self.is_element_present(*LoginPageLocators.LOGIN_FORM), "No login form on page"
+
+    def should_be_register_form(self):
+        assert self.is_element_present(*LoginPageLocators.REGISTER_FORM), "No register form on page"
