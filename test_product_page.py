739cb8ff5fbaf83382dde4afe4f09719028944a5
Abc <roma.baranov@gmail.com>
2020-04-28 17:05:37 +0300

    paramerize test_gest_can_add_product_to_basket

--- test_product_page.py
+++ test_product_page.py
@@ -1,9 +1,15 @@
+import pytest
 from pages.product_page import ProductPage
-import time
 
 
-def test_guest_can_add_product_to_basket(browser):
-    link = "http://selenium1py.pythonanywhere.com/catalogue/the-shellcoders-handbook_209/?promo=newYear"
+@pytest.mark.parametrize('link',
+                         ["http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer" + str(i)
+                          for i in range(10) if i != 7] +
+                         [pytest.param(
+                             "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer7",
+                             marks=pytest.mark.xfail)])
+def test_guest_can_add_product_to_basket(browser, link):
+    # link = "http://selenium1py.pythonanywhere.com/catalogue/the-shellcoders-handbook_209/?promo=newYear"
     page = ProductPage(browser, link)
     page.open()
     page.should_be_add_to_basket_button()
